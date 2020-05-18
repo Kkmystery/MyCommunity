d333e686ac8f235159e8a9c0cc65e9e48a92e62f
kk <1020803064@qq.com>
2020-05-18 10:42:18 +0800

    demo1

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,6 +1,6 @@
 var CONTEXT_PATH="/community";
-// var localhost=" http://47.98.255.128:8080";
- var localhost=" http://localhost:80";
+ var localhost=" http://47.98.255.128:8888";
+ //var localhost=" http://localhost:8080";
 window.alert = function(message) {
 	if(!$(".alert-box").length) {
 		$("body").append(
